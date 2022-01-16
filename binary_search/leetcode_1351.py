3398d3d849d3f90af1e2c4ed0a65cf50e48039bc
saiful00000 <saiful161003@gmail.com>
2022-01-17 01:45:35 +0600

    tryed to solve the problem but failed

--- binary_search/leetcode_1351.py
+++ binary_search/leetcode_1351.py
@@ -1,11 +1,11 @@
 from typing import List
 
 
-def isPosible(v1, v2):
-    return (v1 >= 0 and v2 >= 0)
+def is_possible(v1, v2):
+    return v1 >= 0 and v2 >= 0
 
 
-def count_negatives(grid: List[List[int]]) -> List[int]:
+def count_negatives(grid: List[List[int]]) -> int:
     r_low = 0
     r_up = len(grid) - 1
     c_low = 0
@@ -20,8 +20,12 @@ def count_negatives(grid: List[List[int]]) -> List[int]:
 
         "if negative"
         if grid[r_mid][c_mid] < 0:
-
-            if isPosible(r_mid - 1, c_mid - 1) and grid[r_mid - 1][c_mid - 1] >= 0:
+            if is_possible(r_mid - 1, c_mid - 1):
+                if grid[r_mid - 1][c_mid - 1] >= 0:
+                    required_r_mid = r_mid
+                    required_c_mid = c_mid
+                    break
+            else:
                 required_r_mid = r_mid
                 required_c_mid = c_mid
                 break
@@ -31,10 +35,22 @@ def count_negatives(grid: List[List[int]]) -> List[int]:
             r_low = r_mid + 1
             c_low = c_mid + 1
 
-    return [required_r_mid, required_c_mid]
+    count = 0
 
+    """count all values raw wise"""
+    for i in range(len(grid) - 1, required_r_mid - 1, -1):
+        for j in range(len(grid[0]) - 1, -1, -1):
+            if grid[i][j] < 0:
+                print(grid[i][j])
+                count += 1
 
+    """count column wise"""
+    for i in range(required_r_mid - 1, -1, -1):
+        for j in range(len(grid[0]) - 1, required_c_mid-1, -1):
+            if grid[i][j] < 0:
+                print(grid[i][j])
+                count += 1
 
+    print(f'required row = {required_c_mid}, column = {required_r_mid}')
 
-
-
+    return count
