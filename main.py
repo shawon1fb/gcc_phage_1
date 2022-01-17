fc49d42538f532eff758edc4e1a2c5b8ab77b9ee
saiful00000 <saiful161003@gmail.com>
2022-01-18 00:05:10 +0600

    leet code binary search problem solved

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from binary_search import leetcode_1351
 
 
 def main():
-    print(f'test case 1 : {leetcode_1351.count_negatives(leetcode_1351.test_case_1)}')
-    print(f'test case 2 : {leetcode_1351.count_negatives(leetcode_1351.test_case_2)}')
-    print(f'test case 3 : {leetcode_1351.count_negatives(leetcode_1351.test_case_3)}')
-    print(f'test case 4 : {leetcode_1351.count_negatives(leetcode_1351.test_case_4)}')
-    print(f'test case 5 : {leetcode_1351.count_negatives(leetcode_1351.test_case_5)}')
+    leetcode_1351.run_this_file()
 
 
 if __name__ == '__main__':
